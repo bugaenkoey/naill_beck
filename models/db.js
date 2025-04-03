6b384d92c66aa0b1b130b9369bbc8d4233077269
Yevhen <buhaienkoyevhen@gmail.com>
2025-04-03 22:57:28 +0300

    Conect on MySQL naill_yesterday https://dash.filess.io/#/app/databases/v1/5b459569-576b-445d-a2f1-283b17f189b0

--- models/db.js
+++ models/db.js
@@ -5,13 +5,21 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
+
+console.log(db);
 
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
     return;
   }
+
+  db.query("SELECT 1+1").on("result", function (row) {
+    console.log(row);
+  });
+
   console.log("Connected to MySQL database.");
 });
 
